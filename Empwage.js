6e92c5aef3e68b7e2db914e1366fb433825f7423
“Srujan03076” <srujanmahaja5@gmail.com>
2021-11-21 11:34:09 +0530

    [Srujan]Added code to check employee attendance

--- Empwage.js
+++ Empwage.js
@@ -1,1 +1,16 @@
-Welcome to Empwage JS
+//Welcome to Empwage JS
+
+ //UC1 Checking employee attendance 
+ const IS_ABSENT=0;
+
+ //Using random 
+ let empCheck=Math.floor(Math.random()*10)%2;
+
+ if(empCheck==IS_ABSENT)
+ {
+   console.log("Employee is absent");
+ }
+ else
+ {
+    console.log("Employee is present");
+ }
